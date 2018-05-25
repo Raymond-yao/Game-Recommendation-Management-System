937e3036e9fa4a986a3c3ae3d7f0d35912b7896b
LovecraftZhang <lovecraft.zhang@gmail.com>
2018-05-25 12:04:38 -0700

    Merge branch 'master' of https://github.com/Raymond-yao/Game-Recommendation-Management-System Random login background img # Conflicts: #	public/javascript/index.js #	src/views/index.html

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -32,4 +32,21 @@ $(function () {
       $(".warning-password").css("display", state.password === "" ? "block" : "none")       
     }
   })
-} );
+
+  // random background image
+  var images=['https://img.game.co.uk/ml2/7/2/4/4/724460_scr5_a.png',
+              'https://www.ricedigital.co.uk/wp-content/uploads/2018/04/dark-souls-remastered-preview2.jpg',
+              'https://41zxbw463fq733z1kl101n01-wpengine.netdna-ssl.com/wp-content/uploads/2016/04/Gamespot-1.jpg',
+              'https://i.imgur.com/ZroVEXL.jpg',
+              'https://deow9bq0xqvbj.cloudfront.net/dir-logo/233639/233639.jpg',
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ7ULdp4GylS-Pq1YsLc9kE3gvvUAmqYjkFUOKC5JEFIeKCrgl',
+              'http://handeyesociety.com/wp-content/uploads/2017/02/ComicsXGames2017_CorySchmitz.png',
+              'https://khouj.com/wp-content/uploads/2018/03/game.jpg',
+              'http://escolabrasileiradegames.com.br/wp2016/wp-content/uploads/2017/07/escola-brasileira-de-games-producao-e-desenvolvimento-de-games-2.jpg',
+              ];
+
+  var randomNumber = Math.floor(Math.random() * images.length);
+  var bgImg = 'url(' + images[randomNumber] + ')';
+
+$('body').css({'background':bgImg, 'background-size':'cover', });
+} ());
